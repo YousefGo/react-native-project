48bc7521b452044f513b351d53d47632924e8ae4
yousef <yousef0sarkar@gmail.com>
2023-01-07 03:35:53 +0300

    app of color genetrating

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,15 +1,35 @@
-import React from 'react'; 
-import {Text,View , Button } from 'react-native';
+import React,{useState} from 'react'; 
+import {Text,View , Button ,FlatList } from 'react-native';
 
 const ColorScreen = ()=>{
-
+    const [colors,setColors] =useState([]);
+    const randomColor=()=>{
+        const red = Math.floor(Math.random()*256) ; 
+        const green = Math.floor(Math.random()*256) ; 
+        const blue = Math.floor(Math.random()*256) ; 
+        return `rgb(${red},${green},${blue})`;
+    }
 return (
     <View>
-        <Button  title='Add a Color' />
-
-        
+        <Button  title='Add a Color' onPress={()=>{
+            setColors([...colors,randomColor()]);
+        }} />
+    
+        <FlatList 
+        keyExtractor={(item)=>item}
+        data={colors}
+        renderItem={
+           ({item})=>{
+          return  (
+            <View style={{width:100,height:100,backgroundColor:item}}> 
+            </View>
+          );
+           }}
+        />  
     </View>
 )
+
+
 }
 
 export default ColorScreen ;
